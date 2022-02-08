f1517486d1b44dc04d603ca0fe0584f075f54512
Josie <theblackcatms@gmail.com>
2022-02-08 13:48:50 +0100

    Main_1_1 patched
    Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master Please dont push to /master

--- src/Class01/Main_1_1.java
+++ src/Class01/Main_1_1.java
@@ -3,6 +3,7 @@ package Class01;
 public class Main_1_1 {
     public static void main(String[] arg) {
         System.out.println("Joselyne de Zeeuw");
+        System.out.println("Repost if you pushed this to /main and not /master like the shithead VCS does.");
     }
 
 }
