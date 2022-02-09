49b90712fd589f5f436206840b49c7f6f5f78de5
Josie <theblackcatms@gmail.com>
2022-02-09 16:39:46 +0100

    File reorg and Saw/Sum patched
    This SHOULD work fine. Handed in as suit.

--- src/Practicum01/Exc_1_Sum.java
+++ src/Practicum01/Exc_1_Sum.java
@@ -3,7 +3,7 @@ package Practicum01;
 public class Exc_1_Sum {
     public static void main(String[] args) {
         int total = 0;
-        for (int i=0; i<40; i++) {
+        for (int i = 0; i < 40; i++) {
             total = total + i;
             System.out.println("Current total number at count " + i + ": " + total);
         }
