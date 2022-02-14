261ad979227b06234430b42a7537f4dfc3f11b3a
Josie <theblackcatms@gmail.com>
2022-02-14 11:28:42 +0100

    Practicum 2B completed
    do nothing for 2 days and then magically complete it in 20 minutes man i love java

--- src/Practicum02/Voetbalclub.java
+++ src/Practicum02/Voetbalclub.java
@@ -2,9 +2,12 @@ package Practicum02;
 
 public class Voetbalclub {
     private String name;
+    private int aantalGewonnen;
+    private int aantalGelijk;
+    private int aantalVerloren;
 
-    public Voetbalclub(String name) {
-        this.name = name;
+    public Voetbalclub(String nm) {
+        this.name = nm;
     }
 
     public void verwerkResultaat(char ch) {
@@ -14,5 +17,17 @@ public class Voetbalclub {
             aantalGelijk = aantalGelijk + 1;
         if (ch == 'v')
             aantalVerloren = aantalVerloren + 1;
+    }
+
+    public int aantalPunten() {
+        return aantalGewonnen * 3 + aantalGelijk;
+    }
+
+    public int aantalGespeeld() {
+        return aantalGewonnen + aantalGelijk + aantalVerloren;
+    }
+
+    public String toString() {
+        return name + " " + aantalGespeeld() + " " + aantalGewonnen + " " + aantalGelijk + " " + aantalVerloren + " " + aantalPunten();
     }
 }
