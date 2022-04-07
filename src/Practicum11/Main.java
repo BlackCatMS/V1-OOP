b7930ecbe5073cca83bfa11328b6388e8f76545d
Josie <theblackcatms@gmail.com>
2022-04-07 10:23:46 +0200

    Practicum 11 completed
    If this fuck then proceeds to tell me that I'm either late or that my code is "not what you should've done" I'm literally not even going to bother retaking this what the fuck

--- src/Practicum11/Main.java
+++ src/Practicum11/Main.java
@@ -17,18 +17,32 @@ public class Main {
         Path p1 = Path.of("src/Practicum11/" + originName);
         System.out.println(p1.toAbsolutePath() + "\n");
 
-//        System.out.println("Please insert the destination file below: ");
-//        String destName = keyboardScanner.nextLine() + ".txt";
-//        System.out.println("Destination file: " + destName);
-//        Path p2 = Path.of(destName);
-//        Files.createFile(p2);
-//        System.out.println(p2.toAbsolutePath() + "\n");
+        System.out.println("Please insert the destination file below: ");
+        String destName = keyboardScanner.nextLine() + ".txt";
+        System.out.println("Destination file: " + destName);
+        Path p2 = Path.of("src/Practicum11/"+ destName);
+        Files.createFile(p2);
+        System.out.println(p2.toAbsolutePath() + "\n");
+
+        System.out.println("Please insert the current value of EUR in USD:");
+        Double tradeValue = Double.parseDouble(keyboardScanner.nextLine());
 
         List<String> USDContent = Files.readAllLines(p1.toAbsolutePath());
-        for (String line : USDContent) {
-            System.out.println(line);
-            String[] s = line.split(":");
 
+        if (Files.exists(p2)) {
+            BufferedWriter output = Files.newBufferedWriter(p2);
+            String flow = "";
+            for (String line : USDContent) {
+                String[] s = line.split(" : ");
+                String dollarPrice = s[s.length - 1];
+                Double EURPrice = Double.parseDouble(dollarPrice) * tradeValue;
+
+                flow += s[s.length-2] + " : " + String.format("%.2f", EURPrice) + "\n";
+
+            }
+            output.write(flow);
+            output.close();
+            System.out.println("Conversion success! The converted prices are located at " + p2);
         }
 
 
